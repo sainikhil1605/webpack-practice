83036a3bb137be3585f4ead02b27dacd6824a684
sainikhil1605 <sainikhilvatti1605@gmail.com>
2023-11-08 18:16:31 -0600

    Added loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
 const helloWorlButton = new HelloWorldButton();
+const heading = new Heading();
 helloWorlButton.render();
+heading.render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require("path");
+// Comes with webpack
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    // Browser caches js and css files by file name so if we make changes to code it will not reflect in browser
+    // So we need to change the file name when we make changes to code so that browser will not use the cached file
+    // We can use contenthash to generate a unique hash for the file name
+    // New file is only generated when there is a change in the code
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     // Concatinates this to src of the images or resources in the html file
     // publicPath: "http://some-cdn.com/",
-    publicPath: "dist/",
+    publicPath: "",
+    // Gives same function as a cleanwebpackplugin
+    // clean: {
+    // Dry is used to test the clean plugin if true it will not delete the files but tells us which files will be deleted
+    //   dry: true,
+    // Tells which files to keep
+    //   keep: /\.css/,
+    // },
   },
   mode: "none",
   module: {
@@ -38,7 +54,9 @@ module.exports = {
         //Css loader just loads the css files
         // Style loader takes the css and injects it into the html file
         // Loaders need to be installed seperately
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
+        // Mini css plugin is replaced with style loader
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.scss$/,
@@ -46,7 +64,9 @@ module.exports = {
         // Style loader takes the css and injects it into the html file
         // Loaders need to be installed seperately
         //Loaders are applied from right to left
-        use: ["style-loader", "css-loader", "sass-loader"],
+        // use: ["style-loader", "css-loader", "sass-loader"],
+        // Mini css plugin is replaced with style loader
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
@@ -62,6 +82,33 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
     ],
   },
+  plugins: [
+    // Makes the bundle size smaller
+    new TerserPlugin(),
+    // Extracts the css into a seperate file which helps in not loading css with js and bundle.js size decreases
+    // We can load multiple small files with this
+    new MiniCssExtractPlugin({
+      // [contenthash] is used to generate a unique hash for the file name
+      filename: "styles.[contenthash].css",
+    }),
+    // Cleans the dist folder before generating new files so that we dont have any old files
+    new CleanWebpackPlugin(),
+    // // {
+    // //   cleanOnceBeforeBuildPatterns: [
+    // //     "**/*",
+    // //     path.join(process.cwd(), "build/**/*"),
+    // //   ],
+    // // }
+    new HtmlWebpackPlugin({
+      title: "Hello World",
+      template: "src/index.hbs",
+      description: "Some description",
+    }),
+  ],
 };
